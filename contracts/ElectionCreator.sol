0526c52d4e4dd15ea4dd7982497d1f7ef80915d2
Isidore PORRETTI <isidore.porretti@epfedu.fr>
2021-11-09 14:50:17 +0100

    partie1

--- contracts/ElectionCreator.sol
+++ contracts/ElectionCreator.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.6.8;
 import "../contracts/Election.sol";
 
 contract ElectionCreator {
+
+
+    uint public electionCount;
+
+    event ElectionCreated(address electionAddress);
+    mapping(uint => address) public electionList;
+
     // Creates an election contract and emits the according event
     function newElection(string calldata _name) external {
+
+        electionList[electionCount] = address(new Election(_name));
+        electionCount++;
+        emit ElectionCreated(msg.sender);
     }
 }
---
+++ migrations/2_electionCreator_migration.js
@@ -0,0 +1,9 @@
+var ElectionCreator = artifacts.require("./ElectionCreator.sol");
+module.exports = function(deployer) {
+deployer.deploy(ElectionCreator);
+};
+
+module.exports = function(deployer) {
+    deployer.deploy(ElectionCreator);
+
+};
